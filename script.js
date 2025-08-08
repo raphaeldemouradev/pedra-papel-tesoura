8e43c2571112bcfb855a13539d9c89470609918a
Raphael de Moura <raphaelmoura.dev@gmail.com>
2025-08-07 23:35:09 -0300

    logica add e remoçao de uma pasta

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
-//PEDRA
-var pedra = document.getElementById('pedra')
-pedra.addEventListener('click', clickpedra)
+//pedra
+var stone = document.getElementById('pedra')
+stone.addEventListener('click', function pedra() {
+    var resjogador = document.getElementById('resjogador')
 
-function clickpedra() {
-    resjogador.innerHTML = 'Pedra'
-}
+    escolha = 'Pedra'
 
-//PAPEL
-var papel = document.getElementById('papel')
-papel.addEventListener('click', clickpapel)
+    resjogador.innerHTML = escolha
+})
 
-function clickpapel() {
-    resjogador.innerHTML = 'Papel'
-}
+//papel
+var paper = document.getElementById('papel')
+paper.addEventListener('click', function papel() {
+    var resjogador = document.getElementById('resjogador')
 
-//TESOURA
-var tesoura = document.getElementById('tesoura')
-tesoura.addEventListener('click', clicktesoura)
+    escolha = 'Papel'
 
-function clicktesoura() {
-    resjogador.innerHTML = 'Tesoura'
-}
+    resjogador.innerHTML = escolha
+})
+
+//tesoura
+var cut = document.getElementById('tesoura')
+cut.addEventListener('click', function tesoura() {
+    var resjogador = document.getElementById('resjogador')
+
+    escolha = 'Tesoura'
+
+    resjogador.innerHTML = escolha
+})
+
+//maquina
+var gera = document.getElementById('escolhabot')
+gera.addEventListener('click', function escolhabot() {
+    var resmaquina = document.getElementById('resmaquina')
+    var resultado = document.getElementById('resultado')
+
+    var elementos = ['Pedra', 'Papel', 'Tesoura']
+    var MeusNumeros = Math.floor(Math.random() * elementos.length)
+    var escolhaMaquina = elementos[MeusNumeros]
+
+    resmaquina.innerHTML = escolhaMaquina
+
+    if (escolhaMaquina === escolha) {
+        resultado.innerHTML = 'Empate'
+    } else if ((escolhaMaquina === 'Pedra' && escolha === 'Tesoura')) {
+        resultado.innerHTML = 'Perdeu'
+    } else if ((escolhaMaquina === 'Papel' && escolha === 'Pedra')) {
+        resultado.innerHTML = 'Perdeu'
+    } else if ((escolhaMaquina === 'Tesoura' && escolha === 'Papel')) {
+        resultado.innerHTML = 'Perdeu'
+    } else {
+        resultado.innerHTML = 'Ganhou'
+    }
+})
